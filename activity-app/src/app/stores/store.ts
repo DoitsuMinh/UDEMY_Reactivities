8c5f7abf378d150b1d09cf1a9877f2b17cf0d45d
DoitsuMinh <dminhvu99@gmail.com>
2025-01-22 23:33:06 +0700

    fix name

--- activity-app/src/app/stores/store.ts
+++ activity-app/src/app/stores/store.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
-import CommonStore from "./CommonStore";
+import CommonStore from "./commonStore";
 
 interface Store {
   activityStore: ActivityStore;
